3f0cbc1cd588d5702d7ef84f7af521b4c489af59
Ruan Franklin <ruanfranklinmengo@gmail.com>
2022-08-10 10:55:16 -0300

    Novos arquivos para a estrutura

--- mysite/dashboard/admin.py
+++ mysite/dashboard/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import tbl_Employee
+
 
 # Register your models here.
---
+++ mysite/dashboard/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView
+
+ 
+urlpatterns = [
+   # path("",views.base,name="base"),
+    path("user_login/",views.user_login,name="user_login"),
+    path('export_users_csv/', views.export_users_csv,name="export_users_csv"),  
+    path('Import_csv/', views.Import_csv,name="Import_csv"),  
+     
+]
